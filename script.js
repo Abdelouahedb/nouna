eae6f3af573bfbb7f81a40a42a044787cf560790
Abdelouahedb <120993163+Abdelouahedb@users.noreply.github.com>
2024-12-05 21:21:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the values from the form
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Check if the username and password are correct
+    if (username === "nourimane" && password === "dex") {
+        
+        localStorage.setItem("loggedIn", "true");
+
+        // Redirect to another page if credentials are correct
+        
+        window.location.href = "inside.html";
+
+
+    } else {
+        // Display an error message if credentials are incorrect
+        alert("EH RDI BALK");
+    }
+});
